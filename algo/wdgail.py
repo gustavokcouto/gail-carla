Fix gradient calculation on features pre processing

algo/wdgail.py
@@ -61,17 +61,14 @@ class Discriminator(nn.Module):
             expert_metrics).to(expert_metrics.device)
         mixup_metrics = alpha_metrics * expert_metrics + \
             (1 - alpha_metrics) * policy_metrics
-        mixup_metrics.requires_grad = True
 
         alpha_action = alpha.expand_as(expert_action).to(expert_action.device)
         mixup_action = alpha_action * expert_action + \
             (1 - alpha_action) * policy_action
-        mixup_action.requires_grad = True
 
         mixup_state_features, mixup_state_transformed = self.obs_processor(mixup_state)
-
-        mixup_metrics_features = self.metrics_processor(mixup_metrics)
-        mixup_action_features = self.action_processor(mixup_action)
+        mixup_metrics_features, mixup_metrics_transformed = self.metrics_processor(mixup_metrics)
+        mixup_action_features, mixup_action_transformed = self.action_processor(mixup_action)
         
         mixup_data = torch.cat(
             [mixup_state_features, mixup_metrics_features, mixup_action_features], dim=1)
@@ -81,7 +78,7 @@ class Discriminator(nn.Module):
 
         grad = autograd.grad(
             outputs=disc,
-            inputs=(mixup_state_transformed, mixup_metrics, mixup_action),
+            inputs=(mixup_state_transformed, mixup_metrics_transformed, mixup_action_transformed),
             grad_outputs=ones,
             create_graph=True,
             retain_graph=True,
@@ -117,8 +114,8 @@ class Discriminator(nn.Module):
             policy_action = policy_action.to(self.device)
 
             policy_state_features, _ = self.obs_processor(policy_state)
-            policy_metrics_features = self.metrics_processor(policy_metrics)
-            policy_action_features = self.action_processor(policy_action)
+            policy_metrics_features, _ = self.metrics_processor(policy_metrics)
+            policy_action_features, _ = self.action_processor(policy_action)
 
             policy_d = self.trunk(
                 torch.cat([policy_state_features,  policy_metrics_features, policy_action_features], dim=1))
@@ -131,8 +128,8 @@ class Discriminator(nn.Module):
             expert_action = expert_action.to(self.device)
 
             expert_state_features, _ = self.obs_processor(expert_state)
-            expert_metrics_features = self.metrics_processor(expert_metrics)
-            expert_action_features = self.action_processor(expert_action)
+            expert_metrics_features, _ = self.metrics_processor(expert_metrics)
+            expert_action_features, _ = self.action_processor(expert_action)
 
             expert_d = self.trunk(
                 torch.cat([expert_state_features, expert_metrics_features, expert_action_features], dim=1))
@@ -188,8 +185,8 @@ class Discriminator(nn.Module):
                 policy_action = policy_action.to(self.device)
 
                 policy_state_features, _ = self.obs_processor(policy_state)
-                policy_metrics_features = self.metrics_processor(policy_metrics)
-                policy_action_features = self.action_processor(policy_action)
+                policy_metrics_features, _ = self.metrics_processor(policy_metrics)
+                policy_action_features, _ = self.action_processor(policy_action)
 
                 policy_d = self.trunk(
                     torch.cat([policy_state_features, policy_metrics_features, policy_action_features], dim=1))
@@ -200,8 +197,8 @@ class Discriminator(nn.Module):
                 expert_action = expert_action.to(self.device)
 
                 expert_state_features, _ = self.obs_processor(expert_state)
-                expert_metrics_features = self.metrics_processor(expert_metrics)
-                expert_action_features = self.action_processor(expert_action)
+                expert_metrics_features, _ = self.metrics_processor(expert_metrics)
+                expert_action_features, _ = self.action_processor(expert_action)
 
                 expert_d = self.trunk(
                     torch.cat([expert_state_features, expert_metrics_features, expert_action_features], dim=1))
@@ -223,8 +220,8 @@ class Discriminator(nn.Module):
             self.eval()
 
             state_features, _ = self.obs_processor(state)
-            metrics_features = self.metrics_processor(metrics)
-            action_features = self.action_processor(action)
+            metrics_features, _ = self.metrics_processor(metrics)
+            action_features, _ = self.action_processor(action)
 
             d = self.trunk(
                 torch.cat([state_features, metrics_features, action_features], dim=1))

tools/model.py
@@ -83,7 +83,7 @@ class CNNBase(nn.Module):
 
     def forward(self, obs, metrics):
         obs_features, _ = self.obs_processor(obs)
-        metrics_features = self.metrics_processor(metrics)
+        metrics_features, _ = self.metrics_processor(metrics)
 
         nn_output = self.trunk(torch.cat([obs_features, metrics_features], dim=1))
         critic = nn_output[...,0].unsqueeze(dim=1)
@@ -134,28 +134,27 @@ class ProcessObsFeatures(nn.Module):
 class ProcessMetrics(nn.Module):
     def __init__(self, metrics_shape):
         super(ProcessMetrics, self).__init__()
-        road_option_embedding_dimension = 4
+        road_option_embedding_dimension = 8
         max_road_options = 10
         self.road_option_embedding = nn.Embedding(max_road_options, road_option_embedding_dimension)
-        # self.output_dim = metrics_shape - 1 + road_option_embedding_dimension
-        self.output_dim = metrics_shape
+        self.output_dim = metrics_shape - 1 + road_option_embedding_dimension
 
     def forward(self, metrics):
-        # metrics = [target[0], target[1], speed, int(road_option)]
+        # metrics composition [target[0], target[1], speed, int(road_option)]
 
         # scale target by 1000
-        # target_features = 1000 * metrics[:, 0:2]
+        metrics[:, 0:2] = 1000 * metrics[:, 0:2]
 
         # scale speed by 0.1
-        # speed_features = 0.1 * metrics[:, 2]
+        metrics[:, 2] = 0.1 * metrics[:, 2]
 
-        # scale speed by 0.1
-        # road_option_features = self.road_option_embedding(metrics[:, 3].long())
+        road_option_features = self.road_option_embedding(metrics[:, 3].long())
+        metrics_transformed = metrics[:, :3].clone()
+        metrics_transformed.requires_grad = True
 
-        # metrics_features = torch.cat([target_features, speed_features.unsqueeze(1), road_option_features], dim=1)
+        metrics_transformed = torch.cat([metrics_transformed, road_option_features], dim=1)
 
-        # return speed_features.unsqueeze(1)
-        return metrics
+        return metrics_transformed, metrics_transformed
 
 class ProcessAction(nn.Module):
     def __init__(self, action_shape):
@@ -163,4 +162,7 @@ class ProcessAction(nn.Module):
         self.output_dim = action_shape
 
     def forward(self, action):
-        return action
+        action_transformed = action.clone()
+        action_transformed.requires_grad = True
+
+        return action_transformed, action_transformed