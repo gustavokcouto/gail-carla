1038720d17c4926bc6b1b2e801f3ce054eccb0c9
Gustavo Claudio Karl Couto <gustavokarlcouto@gmail.com>
2021-12-22 14:30:42 -0300

    Add dynamic camera z and meters to pixel calc

--- gen_maps.py
+++ gen_maps.py
@@ -1,7 +1,7 @@
 import queue
 from pathlib import Path
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageColor
 import numpy as np
 
 import carla
@@ -74,7 +74,7 @@ set_sync_mode(client, True)
 
 routes_file = Path('data/routes_training.xml')
 routes = parse_routes_file(routes_file)
-for route_idx in range(10):
+for route_idx in range(len(routes)):
     route = routes[route_idx]
     world = client.load_world(route['town'])
     trajectory = route['trajectory']
@@ -82,32 +82,54 @@ for route_idx in range(10):
     # set_sync_mode(_client, True)
 
     global_plan_gps, global_plan_world_coord = interpolate_trajectory(world, trajectory)
-    trajectory_centroid = carla.Location(0, 0, 0)
-    n_points = 0
+    max_x = global_plan_world_coord[0][0].location.x
+    min_x = global_plan_world_coord[0][0].location.x
+    max_y = global_plan_world_coord[0][0].location.y
+    min_y = global_plan_world_coord[0][0].location.y
     for trajectory_point in global_plan_world_coord:
-        trajectory_centroid += trajectory_point[0].location
-        n_points += 1
+        point_x = trajectory_point[0].location.x
+        point_y = trajectory_point[0].location.y
+        if point_x > max_x:
+            max_x = point_x
+        elif point_x < min_x:
+            min_x = point_x
 
-    trajectory_centroid /= n_points
+        if point_y > max_y:
+            max_y = point_y
+        elif point_y < min_y:
+            min_y = point_y
+
     image_width = 4096
     image_height = 4096
-    camera = Camera(world, image_width, image_height, 90, trajectory_centroid.x, trajectory_centroid.y, 250, -90, 0)
+    camera_fov = 90
+    camera_x = (max_x + min_x) / 2
+    camera_y = (max_y + min_y) / 2
+    traj_radius = max(max_x - camera_x, max_y - camera_y)
+    camera_z = 1.2 * np.tan((90 - camera_fov / 2) * np.pi / 180) * traj_radius
+    camera = Camera(world, image_width, image_height, camera_fov, camera_x, camera_y, camera_z, -90, 0)
     world.tick()
     result = camera.get()
     image = Image.fromarray(result)
     draw = ImageDraw.Draw(image)
     last_point = global_plan_world_coord[0][0].location
-    last_point -= trajectory_centroid
-    meters_to_pixel_x = 8.2
-    meters_to_pixel_y = -8.2
-    last_point_x = meters_to_pixel_x * last_point.y + image_width / 2
-    last_point_y = meters_to_pixel_y * last_point.x + image_height / 2
+    meters_to_pixel_x = image_width / (2 * np.tan(camera_fov * np.pi / 180 / 2) * camera_z)
+    meters_to_pixel_y = -image_height / (2 * np.tan(camera_fov * np.pi / 180 / 2) * camera_z)
+    last_point_x = meters_to_pixel_x * (last_point.y - camera_y) + image_width / 2
+    last_point_y = meters_to_pixel_y * (last_point.x - camera_x) + image_height / 2
+    radius = 20
+    n_points = len(global_plan_world_coord)
+    rgb_diff = -255 / n_points
+    rgb = 255
+    draw.ellipse([last_point_x - radius, last_point_y - radius, last_point_x + radius, last_point_y + radius], fill=(255, int(rgb), 0))
+    rgb += rgb_diff
     for point_idx in range(1, len(global_plan_world_coord)):
         point_loc = global_plan_world_coord[point_idx][0].location
-        point_loc -= trajectory_centroid
-        point_x = meters_to_pixel_x * point_loc.y + image_width / 2
-        point_y = meters_to_pixel_y * point_loc.x + image_height / 2
-        draw.line((last_point_x, last_point_y, point_x, point_y), width=20, fill=128)
+        point_x = meters_to_pixel_x * (point_loc.y - camera_y) + image_width / 2
+        point_y = meters_to_pixel_y * (point_loc.x - camera_x) + image_height / 2
+        draw.line((last_point_x, last_point_y, point_x, point_y), width=20, fill=(255, int(rgb), 0))
+        rgb += rgb_diff
         last_point_x = point_x
         last_point_y = point_y
+    draw.ellipse([last_point_x - radius, last_point_y - radius, last_point_x + radius, last_point_y + radius], fill=(255, int(rgb), 0))
+
     image.save('trajs/route_%02d.png' % route_idx)
