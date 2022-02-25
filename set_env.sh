cbab1159e170ef095a2b14d9d7ca7d1c470bfff2
Gustavo Claudio Karl Couto <gustavokarlcouto@gmail.com>
2022-02-25 15:36:03 -0300

    Include directories with carla different api version

--- set_env.sh
+++ set_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PYTHONPATH=$PYTHONPATH:/opt/carla-simulator/PythonAPI/carla/
-export PYTHONPATH=$PYTHONPATH:/opt/carla-simulator/PythonAPI/carla/dist/carla-0.9.11-py3.7-linux-x86_64.egg
+export PYTHONPATH=$PYTHONPATH:./api_carla/9.10/PythonAPI/carla/
+export PYTHONPATH=$PYTHONPATH:./api_carla/9.10/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
 screen -L -S carla_train .venv/bin/python wdail_carla.py
